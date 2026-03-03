14d6775b0e2f16a560d728b747264691f002c6bf
DeepuAP <167683781+DeepuAP@users.noreply.github.com>
2026-03-03 09:40:17 +0530

    Update Powersheell_Task3_Kiransir.ps1

--- Powersheell_Task3_Kiransir.ps1
+++ Powersheell_Task3_Kiransir.ps1
@@ -1,11 +1,11 @@
 # Get failed logon events from last 24 hours
 $events = Get-WinEvent -FilterHashtable @{
-    LogName = 'Security'
-    Id = 4625
+    LogName   = 'Security'
+    Id        = 4625
     StartTime = (Get-Date).AddHours(-24)
 }
 
-# Extract account and IP
+# Extract Account and IP Address
 $data = foreach ($event in $events) {
     [PSCustomObject]@{
         Account = $event.Properties[5].Value
@@ -13,18 +13,27 @@ $data = foreach ($event in $events) {
     }
 }
 
-# Top 10 accounts
+# Top 10 Accounts
 $topAccounts = $data |
+Where-Object { $_.Account -and $_.Account -ne "-" } |
 Group-Object Account |
 Sort-Object Count -Descending |
 Select-Object -First 10
 
 # Top 10 IPs
 $topIPs = $data |
+Where-Object { $_.IP -and $_.IP -ne "-" } |
 Group-Object IP |
 Sort-Object Count -Descending |
 Select-Object -First 10
 
-# Export reports
-$topAccounts | Export-Csv "TopFailedAccounts.csv" -NoTypeInformation
-$topIPs | Export-Csv "TopFailedIPs.csv" -NoTypeInformation
+# Export Clean Reports (Important Fix Here)
+$topAccounts |
+Select-Object Name, Count |
+Export-Csv "TopFailedAccounts.csv" -NoTypeInformation
+
+$topIPs |
+Select-Object Name, Count |
+Export-Csv "TopFailedIPs.csv" -NoTypeInformation
+
+Write-Host "Report Generated Successfully"
